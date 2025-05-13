b7b94fd194811978d493642149cc1cd588deca0b
Kalyan Tsk <se22uari192@mahindrauniversity.edu.in>
2025-05-13 23:15:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+window.env = {
+  BACK4APP_APPLICATION_ID: 'xHzlhGDAokVwkkmTs8kQZ4hYnUpVSB27GBlIL7rU',
+  BACK4APP_REST_API_KEY: 'jdaPKG9qFUysFuoCNkxt9F15zX9eFbhR9HLPDw75',
+  BACK4APP_BASE_URL: 'https://parseapi.back4app.com/classes/Review'
+};
